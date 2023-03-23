637ae83bdbca9554febd78cdc10d361e2d367d03
RinaBoni <brisovae@mail.ru>
2023-03-23 12:49:16 +0900

    переместила задачки в отдельную папку

---
+++ python/mathematical task/lab6.py
@@ -0,0 +1,19 @@
+#https://ivtipm.github.io/Programming/Glava07/index07.htm#z17810
+# последовательности a1,...,an:
+#б) кратных 3 и не кратных 5;
+
+__author__ ="Borisova Ekaterina IVT20"
+
+import ar
+import mth
+
+n =int(input ("Введите количество элементов в массиве: "))
+
+a=[]
+
+a = (ar.ar_filling(a, n))
+
+
+print(a)
+#print(ar6.calc(a))
+print(f"Результат равен: {mth.calc6(a)}")
