247dd751ad42b0d3dc87aef97dc3178df3cc333c
RinaBoni <brisovae@mail.ru>
2023-04-02 08:25:12 +0900

    1

--- numpy + matplotlib + seaborn + pandas/matrix.py
+++ numpy + matplotlib + seaborn + pandas/matrix.py
@@ -7,18 +7,17 @@ i = 50
 j = -50
 
 #создаем и заполняем матрицу
-matrix = np.random.uniform(low=-50, high=50, size=(10,10))
+matrix = np.random.uniform(low=-1, high=1, size=(10,10))
 #создаем тепловую карту
-#sns.heatmap(matrix, annot = True, fmt='.1g',  vmin=-50, vmax=50, center= 0)
+sns.heatmap(matrix, annot = True, fmt='.1g',  vmin=-1, vmax=1, center= 0)
 #показываем тепловую карту
-#plt.show()
+plt.show()
 
+#матрицу в массив
 a = matrix.flatten()
-
-print (a)
-
+#создаем гистограму 
 sns.distplot(a, hist=True, kde=False, 
              bins=int(180/5), color = 'blue',
              hist_kws={'edgecolor':'black'})
-
+#показываем гистограму (покажется после закрытия тепловой карты)
 plt.show()
