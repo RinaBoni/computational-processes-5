e95ee3bb8dee791fe325bf624542d15522e24fc0
RinaBoni <83748388+RinaBoni@users.noreply.github.com>
2023-05-15 16:49:23 +0900

    Update lab1.py

--- mathematical task/lab1.py
+++ mathematical task/lab1.py
@@ -9,14 +9,14 @@ import math as m
 import mth
 
 assert 0 ==mth.calc1(1, 1)
-assert -0.03225806451612903 ==mth.calc1(5, 6)
-assert 0.21428571428571427 ==mth.calc1(-9, 3)
+assert 1.0 ==mth.calc1(1, 0)
+assert -0.125 ==round(mth.calc1(-3, 5), 3)
 
 x = float (input("Введите x: "))
 y = float (input("Введите y: "))
 
 result = mth.calc1(x, y)
 
-print (f"Результат  {result: .3f}")
+print (f"Результат  {result: }")
 #.3f
 
